6b6cef007c845e54dda8acab8d2b431a55389c9b
CesValde <valderramacesar2002@gmail.com>
2026-02-08 15:20:39 -0300

    add: endpoint
    resetPassword endpoint

--- src/routes/notify.route.js
+++ src/routes/notify.route.js
@@ -2,12 +2,13 @@ import { Router } from "express"
 import path from "path"
 import { passportCall } from "../middleware/auth.middleware.js"
 import * as notifyController from "../controllers/notify.controller.js"
+import * as userController from "../controllers/users.controller.js"
 
 const router = Router()
 
 router.get("/mail", passportCall("jwt"), notifyController.sendMail)
 
-router.get("/change-password", (req, res) => {
+router.get("/change-password/:token", (req, res) => {
    res.sendFile(path.join(process.cwd(), "src/public/html/changePassword.html"))
 })
 
