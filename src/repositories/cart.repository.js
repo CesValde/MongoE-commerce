e9970be47b9f8f7f81cc7b7f418981bd26e8e93b
CesValde <valderramacesar2002@gmail.com>
2026-02-13 11:48:34 -0300

    add: function in repository
    cart

--- src/repositories/cart.repository.js
+++ src/repositories/cart.repository.js
@@ -1,6 +1,15 @@
 import cartsDAO from "../daos/cart.dao.js"
 
 class CartRepository {
+   async getAll() {
+      return await cartsDAO.getAll()
+   }
+
+   // only to show the user
+   async getByIdByPopulated(id) {
+      return await cartsDAO.getByIdByPopulated(id)
+   }
+
    async getById(id) {
       return await cartsDAO.getById(id)
    }
